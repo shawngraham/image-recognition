3cadb70885976e7292183fa061fde835fbf7f4a0
Shawn Graham <shawn_graham@carleton.ca>
2019-03-07 14:42:19 -0500

    Update get-pics.py

--- get-pics.py
+++ get-pics.py
@@ -7,7 +7,7 @@ with open('output.txt') as csvfile:
     csvrows = csv.reader(csvfile, delimiter=',', quotechar='"')
     for row in csvrows:
         filename = row[0]
-        filename = re.sub('["https://www.cambridgeairphotos.com/data/thumbnails"]', '', filename)
+        filename = re.sub('["\bhttps://www.cambridgeairphotos.com/data/thumbnails\b"]', '', filename)
         url = row[0]
         print(url)
         result = requests.get(url, stream=True)
